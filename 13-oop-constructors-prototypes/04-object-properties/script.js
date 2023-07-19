5d395790235a11cca3b98c5ce85ec79a04c245a8
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-07-19 19:04:41 +0200

    I learned about some object properties

--- 13-oop-constructors-prototypes/04-object-properties/script.js
+++ 13-oop-constructors-prototypes/04-object-properties/script.js
@@ -6,3 +6,36 @@ function Rectangle(name, width, height) {
     return this.width * this.height;
   };
 }
+
+const rect1 = new Rectangle('Rectangle 1', 20, 20);
+const rect2 = new Rectangle('Rectangle 2', 30, 30);
+
+console.log(rect1.name, rect2.width);
+console.log(rect2['name']);
+
+// Add property
+rect1.color = 'red';
+console.log(rect1);
+
+rect2.perimeter = () => 2 * (rect2.width + rect2.height);
+
+console.log(rect2.perimeter());
+
+// Delete property
+delete rect2.perimeter;
+
+// Check for property
+console.log(rect2.hasOwnProperty('color'));
+console.log(rect1.hasOwnProperty('color'));
+
+// Get keys
+console.log(Object.keys(rect1));
+console.log(Object.values(rect1));
+console.log(Object.entries(rect1));
+
+for (let [key, value] of Object.entries(rect1)) {
+  if (typeof value !== 'function') {
+    console.log(key, value);
+  }
+}
+console.log(rect2);
