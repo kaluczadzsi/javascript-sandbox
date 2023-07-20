edff6f6c3179707fe1e2343cab9ecf816584d75c
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-07-20 19:13:38 +0200

    I learned about classes

--- 14-oop-classes-private-properties/01-classes/script.js
+++ 14-oop-classes-private-properties/01-classes/script.js
@@ -1,20 +1,48 @@
-function Rectangle(height, width) {
-  this.height = height;
-  this.width = width;
+// function Rectangle(height, width) {
+//   this.height = height;
+//   this.width = width;
+// }
+
+// Rectangle.prototype.area = function () {
+//   return this.height * this.width;
+// };
+
+// Rectangle.prototype.perimeter = function () {
+//   return 2 * (this.height + this.width);
+// };
+
+// Rectangle.prototype.isSquare = function () {
+//   return this.height === this.width;
+// };
+
+// Rectangle.prototype.logArea = function () {
+//   console.log('Rectangle Area: ' + this.area());
+// };
+
+class Rectangle {
+  constructor(name, height, width) {
+    this.name = name;
+    this.height = height;
+    this.width = width;
+  }
+
+  area() {
+    return this.height * this.width;
+  }
+
+  perimeter() {
+    return 2 * (this.height + this.width);
+  }
+
+  isSquare() {
+    return this.height === this.width;
+  }
+
+  logArea() {
+    console.log('Rectangle Area: ' + this.area());
+  }
 }
 
-Rectangle.prototype.area = function () {
-  return this.height * this.width;
-};
-
-Rectangle.prototype.perimeter = function () {
-  return 2 * (this.height + this.width);
-};
-
-Rectangle.prototype.isSquare = function () {
-  return this.height === this.width;
-};
-
-Rectangle.prototype.logArea = function () {
-  console.log('Rectangle Area: ' + this.area());
-};
+const square = new Rectangle('Square', 20, 20);
+console.log(square);
+square.logArea();
