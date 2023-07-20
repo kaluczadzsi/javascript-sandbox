963b69d6a8a2fb497c52ab0c5a2bf46a39cecded
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-07-20 20:09:03 +0200

    I learned getters and setters

--- 14-oop-classes-private-properties/04-getters-setters-classes/script.js
+++ 14-oop-classes-private-properties/04-getters-setters-classes/script.js
@@ -1,15 +1,39 @@
-class App {
-  constructor() {
-    this.serverName = 'localhost';
-
-    document
-      .querySelector('button')
-      .addEventListener('click', this.getServerName.bind(this));
+class Person {
+  constructor(firstName, lastName) {
+    this._firstName = firstName;
+    this._lastName = lastName;
   }
 
-  getServerName() {
-    console.log(this.serverName);
+  get firstName() {
+    return this.capitalizeFirst(this._firstName);
+  }
+
+  get lastName() {
+    return this.capitalizeFirst(this._lastName);
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  set firstName(value) {
+    this._firstName = this.capitalizeFirst(value);
+  }
+
+  set lastName(value) {
+    this._lastName = this.capitalizeFirst(value);
+  }
+
+  capitalizeFirst(value) {
+    return value.charAt(0).toUpperCase() + value.slice(1);
   }
 }
 
-const app = new App();
+const person1 = new Person('Uchiha', 'Obito');
+console.log(person1.firstName);
+console.log(person1.lastName);
+
+person1.firstName = 'Tomas';
+person1.lastName = 'Dorominy';
+
+console.log(person1.fullName);
