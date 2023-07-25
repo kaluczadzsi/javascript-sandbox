5b695ed150930807a8617fb2ad128afb7ac6a5a9
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-07-25 16:57:50 +0200

    I learned Set

---
+++ 17-iterators-data-structures/05-sets/script.js
@@ -0,0 +1,23 @@
+const set = new Set([1, 2, 2, 3, 4, 4]);
+
+console.log(set);
+
+set.add(5);
+console.log(set.has(3));
+
+set.delete(2);
+set.has(2);
+console.log(set);
+
+const setArray = Array.from(set);
+console.log(setArray);
+
+for (const item of set) {
+  console.log(item);
+}
+
+const iterator = set.values();
+
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
